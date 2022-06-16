29b521296f3bfcb7d2f0672981c6fdfe70246ba9
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2022-06-16 23:02:18 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 /**
- * struct stack_t - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
@@ -16,7 +16,7 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct stack_t
+typedef struct stack_s
 {
 	int n;
 	struct stack_s *prev;
@@ -24,14 +24,14 @@ typedef struct stack_t
 } stack_t;
 
 /**
- * struct instruction_t - opcode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct instruction_t
+typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
